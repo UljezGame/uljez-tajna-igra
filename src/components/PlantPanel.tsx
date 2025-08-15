ea287e9476423a09044db3d412c5ff70ddc8e714
DinoZlf <zulfikaridino5@gmail.com>
2025-08-15 18:42:36 +0200

    Html plant panel changes

--- src/components/PlantPanel.tsx
+++ src/components/PlantPanel.tsx
@@ -50,7 +50,7 @@ export const PlantPanel = ({ partyCode, players, isHost }: PlantPanelProps) => {
       <CardHeader>
         <CardTitle className="flex items-center gap-2">
           <Sprout className="w-5 h-5 text-green-600" />
-          Plant Feature
+          Plant
         </CardTitle>
       </CardHeader>
       <CardContent className="space-y-4">
